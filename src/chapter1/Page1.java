3e59f20c7da07a47a05ecfee958907ca6753349d
Roam <roman.gribov94@gmail.com>
2024-11-24 18:51:00 +0300

    added file Page2

--- src/chapter1/Page1.java
+++ src/chapter1/Page1.java
@@ -1,3 +1,5 @@
+package chapter1;
+
 public class Page1 {
     public static void main(String[] args) {
         System.out.println("Roman added this line");
---
+++ src/chapter1/Page2.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+public class Page2 {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 10; i++) {
+
+            System.out.println("The page 2 has " + i + " paragraphes");
+
+
+        }
+    }
+}
